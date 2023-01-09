252d2cd8dea41014b30c1da4fe9d5dc2b6ebfeaa
Sultanbek <soultouchka@gmail.com>
2023-01-09 16:30:44 +0600

    feat: l9

--- pkg/handler/middleware.go
+++ pkg/handler/middleware.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strings"
@@ -21,6 +22,7 @@ func (h *Handler) userIdentity(c *gin.Context) {
 	headerParts := strings.Split(header, " ")
 	if len(headerParts) != 2 {
 		newErrorResponse(c, http.StatusUnauthorized, "invalid auth header")
+		return
 	}
 
 	userId, err := h.services.Authorization.ParseToken(headerParts[1])
@@ -30,4 +32,20 @@ func (h *Handler) userIdentity(c *gin.Context) {
 	}
 
 	c.Set(userCtx, userId)
+}
+
+func getUserId(c *gin.Context) (int, error) {
+	id, ok := c.Get(userCtx)
+	if !ok {
+		newErrorResponse(c, http.StatusInternalServerError, "user id not found")
+		return 0, errors.New("user id not found")
+	}
+
+	idInt, ok := id.(int)
+	if !ok {
+		newErrorResponse(c, http.StatusInternalServerError, "user id is of invalid type")
+		return 0, errors.New("user id is of invalid type")
+	}
+
+	return idInt, nil
 }
--- todo.go
+++ todo.go
@@ -1,9 +1,9 @@
 package todo
 
 type TodoList struct {
-	Id          int `json:"id"`
-	Title       int `json:"title"`
-	Description int `json:"description"`
+	Id          int    `json:"id" db:"id"`
+	Title       string `json:"title" db:"title" binding:"required"`
+	Description string `json:"description" db:"description"`
 }
 
 type UsersList struct {
